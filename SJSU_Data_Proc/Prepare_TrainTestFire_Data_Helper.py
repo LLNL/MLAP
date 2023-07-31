44f26cc9d2e363ce243990ea1f6e9e9778d63ef9
Pankaj Jha <jha3@llnl.gov>
2023-07-30 22:19:39 -0700

    Implement computing of wind magnitude and dropping wind components

--- SJSU_Data_Proc/Prepare_TrainTestFire_Data_Helper.py
+++ SJSU_Data_Proc/Prepare_TrainTestFire_Data_Helper.py
@@ -39,12 +39,33 @@ def get_keys_from_extracted_data (df_extracted):
     keys_SW = [key for key in keys_all if 'SW' in key]
     keys_HGT = [key for key in keys_all if 'HGT' in key]
     
-    keys_U10Mag = []
+    keys_UMag10 = []
     for U10_key, V10_key in zip(keys_U10, keys_V10):
         assert U10_key[3:] == V10_key[3:]
         UMag10_key = 'UMag10' + U10_key[3:]
-        keys_U10Mag.append(UMag10_key)
+        keys_UMag10.append(UMag10_key)
                      
     print('=========================================================================')
-    return keys_identity, keys_FM, keys_U10, keys_V10, keys_U10Mag, \
+    return keys_identity, keys_FM, keys_U10, keys_V10, keys_UMag10, \
                             keys_T2, keys_RH, keys_PREC, keys_SW, keys_HGT
+
+
+# []
+'''
+Compute UMag from U and V
+'''
+def compute_wind_mag (df_extracted, keys_U10, keys_V10, keys_UMag10):
+    for U10_key, V10_key, UMag_key in zip(keys_U10, keys_V10, keys_UMag10):
+        assert U10_key[3:] == V10_key[3:]
+        df_extracted[UMag_key] = (df_extracted[U10_key]**2 + df_extracted[V10_key]**2)**(0.5)
+
+    return df_extracted
+    
+# []
+'''
+Drop wind components U, and v
+'''
+def drop_wind_components (df_extracted, keys_U10, keys_V10):
+    df_extracted = df_extracted.drop(keys_U10 + keys_V10, axis = 'columns')
+    return df_extracted
+
